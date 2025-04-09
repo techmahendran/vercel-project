d4e0bd9e77ea92de5356812f650137e9d928ff56
techvenkatvis <venkatvis003@gmail.com>
2025-04-09 21:26:06 +0530

    vercel check

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+     <h1 className=' text-2xl font-bold text-center mt-5'>Hi Vercel</h1> 
+    </>
+  )
+}
+
+export default Header
