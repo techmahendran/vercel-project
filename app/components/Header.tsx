244112efd18ac2e823771d0ac5952b0833064d08
techvenkatvis <venkatvis003@gmail.com>
2025-04-10 18:38:30 +0530

    vercel test changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-     <h1 className=' text-2xl font-bold text-center mt-5 bg-red-500 text-white'>Hi Vercel</h1> 
+      <h1 className=" text-2xl font-bold text-center mt-5 bg-red-500 text-white">Hi Vercel Test</h1>
     </>
-  )
+  );
 }
 
 export default Header
